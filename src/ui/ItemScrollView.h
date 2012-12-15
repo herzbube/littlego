521408389d80d6ee12a9177cab7bbf4ffebfcc90
Patrick Näf <herzbube@herzbube.ch>
2012-12-15 21:12:01 +0100

    add ItemScrollView class as prerequisite for displaying a list of moves

---
+++ src/ui/ItemScrollView.h
@@ -0,0 +1,138 @@
+// -----------------------------------------------------------------------------
+// Copyright 2012 Patrick Näf (herzbube@herzbube.ch)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// -----------------------------------------------------------------------------
+
+
+// Forward declarations
+@class ItemScrollView;
+
+
+// -----------------------------------------------------------------------------
+/// @brief Enumerates the orientations supported by ItemScrollView.
+// -----------------------------------------------------------------------------
+enum ItemScrollViewOrientation
+{
+  ItemScrollViewOrientationHorizontal,
+  ItemScrollViewOrientationVertical
+};
+
+// -----------------------------------------------------------------------------
+/// @brief The delegate of ItemScrollView must adopt the ItemScrollViewDelegate
+/// protocol. The delegate is responsible for handling all non-scrolling user
+/// interaction.
+// -----------------------------------------------------------------------------
+@protocol ItemScrollViewDelegate
+@optional
+- (void) itemScrollView:(ItemScrollView*)itemScrollView willDisplayItemView:(UIView*)itemView;
+@end
+
+// -----------------------------------------------------------------------------
+/// @brief The data source of ItemScrollView must adopt the
+/// ItemScrollViewDataSource protocol. The data source is responsible for
+/// providing new item views when they are requested by
+/// ItemScrollViewDataSource.
+// -----------------------------------------------------------------------------
+@protocol ItemScrollViewDataSource
+/// @brief This method is invoked once to calculate the scroll view's content
+/// size.
+- (int) numberOfItemsInItemScrollView:(ItemScrollView*)itemScrollView;
+/// @brief This method is invoked once to calculate the scroll view's content
+/// size.
+- (int) itemWidthOrHeightInItemScrollView:(ItemScrollView*)itemScrollView;
+- (UIView*) itemScrollView:(ItemScrollView*)itemScrollView itemViewAtIndex:(int)index;
+@end
+
+
+// -----------------------------------------------------------------------------
+/// @brief The ItemScrollView class provides seamless (i.e. not paginated)
+/// scrolling through a finite number of item views. The item views are arranged
+/// either horizontally or vertically.
+///
+/// ItemScrollView is designed to be used similarly to UITableView: It requires
+/// that a data source (ItemScrollViewDataSource) provides the item views to be
+/// displayed, and a delegate (ItemScrollViewDelegate) to handle all
+/// non-scrolling user interaction.
+///
+/// Item views must all be of uniform width (if the ItemScrollView orientation
+/// is horizontal) or height (if the ItemScrollView orientation is vertical) so
+/// that the content size and with it the scroll bars can be properly
+/// calculated. The uniform width/height that is advertised by the data source
+/// is neither checked nor enforced when concrete item views are requested. If
+/// the total width/height of all item views exceeds the calculated content
+/// size, some item views are not displayed. If the total width/height is below
+/// the content size, a part of the ItemScrollView remains empty.
+///
+/// In the other direction, item views should match the height/width of the
+/// ItemScrollView. Item views that are less high/wide are placed so that they
+/// align at the top/left of the ItemScrollView. Item views that exceed the
+/// height/width of the ItemScrollView are clipped.
+///
+///
+/// @par Memory usage vs. performance
+///
+/// ItemScrollView requests item views from the data source only as they are
+/// needed: When it is initially displayed, it requests views until they fill
+/// the entire visible area. When the user starts scrolling, ItemScrollView
+/// requests as many views as are needed to fill the area that has become
+/// visible through the scrolling action. Item views that are no longer visible
+/// are removed from ItemScrollView, which means that unless someone else keeps
+/// a reference to them they will be deallocated. This is to keep memory usage
+/// low even if the data source has a large number of item views. The downside
+/// of this is a certain performance overhead that may become noticeable if the
+/// user is scrolling quickly and a large number of new item views needs to be
+/// created in quick succession. For this reason it is recommended to make item
+/// views as light-weight as possible.
+/// 
+///
+/// @par Credits
+///
+/// This class is a complete rewrite of Apple's Street Scroller demo from their
+/// UIScrollView presentation at WWDC 2011. The main goal was to generalize the
+/// concept of a seamless scrolling view:
+/// - It should be possible to scroll any type and number of item views
+/// - Scrolling should be possible both horizontally and vertically
+///
+/// The original demo code can be found here:
+/// http://developer.apple.com/library/ios/samplecode/StreetScroller/Introduction/Intro.html#//apple_ref/doc/uid/DTS40011102
+/// I found the reference to the demo at StackOverflow:
+/// http://stackoverflow.com/questions/6736295/iphone-uiscrollview-continuous-circular-scrolling
+// -----------------------------------------------------------------------------
+@interface ItemScrollView : UIScrollView <UIScrollViewDelegate>
+{
+}
+
+- (id) initWithFrame:(CGRect)frame;
+- (id) initWithFrame:(CGRect)frame orientation:(enum ItemScrollViewOrientation)orientation;
+
+/// @brief The orientation of the ItemScrollView, i.e. in which direction should
+/// scrolling be enabled.
+@property(nonatomic, assign, readonly) enum ItemScrollViewOrientation itemScrollViewOrientation;
+/// @brief The delegate for the ItemScrollView.
+///
+/// Setting a delegate is optional.
+@property(nonatomic, assign) id<ItemScrollViewDelegate> itemScrollViewDelegate;
+/// @brief The data source for the ItemScrollView.
+///
+/// Setting this property reconfigures the view with a new content size and
+/// discards all item views that are currently visible. A new query cycle will
+/// begin to fill the visible area of ItemScrollView with item views as soon as
+/// UIKit begins updating ItemScrollView.
+@property(nonatomic, assign) id<ItemScrollViewDataSource> itemScrollViewDataSource;
+/// @brief The view that is the superview of all item views.
+///
+/// This property is exposed to facilitate zooming by a controller.
+@property(nonatomic, retain, readonly) UIView* itemContainerView;
+
+@end
---
+++ src/ui/ItemScrollView.m
@@ -0,0 +1,451 @@
+// -----------------------------------------------------------------------------
+// Copyright 2012 Patrick Näf (herzbube@herzbube.ch)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// -----------------------------------------------------------------------------
+
+
+// Project includes
+#import "ItemScrollView.h"
+
+
+// -----------------------------------------------------------------------------
+/// @brief Class extension with private methods for ItemScrollView.
+// -----------------------------------------------------------------------------
+@interface ItemScrollView()
+/// @name Initialization and deallocation
+//@{
+- (void) dealloc;
+//@}
+/// @name Overrides from superclass
+//@{
+- (void) layoutSubviews;
+//@}
+/// @name Helpers
+//@{
+- (void) setupItemContainerView;
+- (void) removeAllVisibleItems;
+- (void) updateContentSize;
+- (void) updateVisibleAreaWithMinimumEdge:(CGFloat)minimumEdge maximumEdge:(CGFloat)maximumEdge;
+- (void) updateVisibleAreaAtMaximumEdge:(CGFloat)maximumVisible;
+- (void) updateVisibleAreaFromMinimumEdge:(CGFloat)minimumVisible;
+- (void) removeItemsAfterMaximumEdge:(CGFloat)maximumVisible;
+- (void) removeItemsBeforeMinimumEdge:(CGFloat)maximumVisible;
+- (UIView*) firstItemView;
+- (UIView*) nextItemView;
+- (UIView*) previousItemView;
+- (CGFloat) placeItemView:(UIView*)itemView withMinimumEdgeAt:(CGFloat)position;
+- (CGFloat) placeItemView:(UIView*)itemView withMaximumEdgeAt:(CGFloat)position;
+//@}
+/// @name Privately declared properties
+//@{
+@property(nonatomic, assign) int numberOfItemsInItemScrollView;
+@property(nonatomic, retain) NSMutableArray* visibleItems;
+//@}
+/// @name Re-declaration of properties to make them readwrite privately
+//@{
+@property(nonatomic, assign, readwrite) enum ItemScrollViewOrientation itemScrollViewOrientation;
+@property(nonatomic, retain, readwrite) UIView* itemContainerView;
+//@}
+@end
+
+
+@implementation ItemScrollView
+
+@synthesize itemScrollViewOrientation;
+@synthesize itemScrollViewDelegate;
+@synthesize itemScrollViewDataSource;
+@synthesize numberOfItemsInItemScrollView;
+@synthesize visibleItems;
+@synthesize itemContainerView;
+
+
+// -----------------------------------------------------------------------------
+/// @brief Initializes an ItemScrollView object with frame rectangle @a frame
+/// and items arranged horizontally.
+// -----------------------------------------------------------------------------
+- (id) initWithFrame:(CGRect)frame
+{
+  // Invoke designated initializer
+  return [self initWithFrame:frame orientation:ItemScrollViewOrientationHorizontal];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Initializes an ItemScrollView object with frame rectangle @a frame
+/// and items arranged according to @a orientation.
+///
+/// @note This is the designated initializer of ItemScrollView.
+// -----------------------------------------------------------------------------
+- (id) initWithFrame:(CGRect)frame orientation:(enum ItemScrollViewOrientation)orientation;
+{
+  // Call designated initializer of superclass (UIScrollView)
+  self = [super initWithFrame:frame];
+  if (! self)
+    return nil;
+
+  // Will be set to the correct size when the data source is configured
+  self.contentSize = self.frame.size;
+
+  itemScrollViewOrientation = orientation;
+  itemScrollViewDelegate = nil;
+  itemScrollViewDataSource = nil;
+  numberOfItemsInItemScrollView = 0;
+  visibleItems = [[NSMutableArray alloc] init];
+  [self setupItemContainerView];
+
+  return self;
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Deallocates memory allocated by this ItemScrollView object.
+// -----------------------------------------------------------------------------
+- (void) dealloc
+{
+  itemScrollViewDelegate = nil;
+  itemScrollViewDataSource = nil;
+  // Using self triggers the setter which releases the object
+  self.visibleItems = nil;
+  self.itemContainerView = nil;
+  [super dealloc];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Creates and sets up the container view that will be the item views'
+/// superview.
+///
+/// This is an internal helper invoked during initialization.
+// -----------------------------------------------------------------------------
+- (void) setupItemContainerView
+{
+  itemContainerView = [[UIView alloc] init];
+  itemContainerView.frame = CGRectMake(0, 0, self.contentSize.width, self.contentSize.height);
+  [self addSubview:itemContainerView];
+
+  [itemContainerView setUserInteractionEnabled:NO];
+}
+
+// -----------------------------------------------------------------------------
+// Property is documented in the header file.
+// -----------------------------------------------------------------------------
+- (void) setItemScrollViewDataSource:(id<ItemScrollViewDataSource>)dataSource
+{
+  if (nil == dataSource)
+  {
+    NSException* exception = [NSException exceptionWithName:NSInvalidArgumentException
+                                                     reason:@"Data source must not be nil"
+                                                   userInfo:nil];
+    @throw exception;
+  }
+  itemScrollViewDataSource = dataSource;
+
+  [self removeAllVisibleItems];
+  [self updateContentSize];
+  [self setNeedsDisplay];  // force layout update
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for setItemScrollViewDataSource:()
+// -----------------------------------------------------------------------------
+- (void) removeAllVisibleItems
+{
+  for (NSArray* itemArray in visibleItems)
+  {
+    UIView* itemView = [itemArray objectAtIndex:0];
+    [itemView removeFromSuperview];
+  }
+  [visibleItems removeAllObjects];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for setItemScrollViewDataSource:()
+// -----------------------------------------------------------------------------
+- (void) updateContentSize
+{
+  numberOfItemsInItemScrollView = [itemScrollViewDataSource numberOfItemsInItemScrollView:self];
+  int itemWidthOrHeight = [itemScrollViewDataSource itemWidthOrHeightInItemScrollView:self];
+  CGFloat contentWidthOrHeight = numberOfItemsInItemScrollView * itemWidthOrHeight;
+  if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+    self.contentSize = CGSizeMake(contentWidthOrHeight, self.frame.size.height);
+  else
+    self.contentSize = CGSizeMake(self.frame.size.width, contentWidthOrHeight);
+  itemContainerView.frame = CGRectMake(0, 0, self.contentSize.width, self.contentSize.height);
+}
+
+// -----------------------------------------------------------------------------
+/// @brief This overrides the superclass implementation. Triggers acquisition
+/// of item views.
+// -----------------------------------------------------------------------------
+- (void) layoutSubviews
+{
+  [super layoutSubviews];
+
+  CGRect visibleBounds = [self convertRect:self.bounds toView:itemContainerView];
+  CGFloat minimumVisible;
+  CGFloat maximumVisible;
+  if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+  {
+    minimumVisible = CGRectGetMinX(visibleBounds);
+    maximumVisible = CGRectGetMaxX(visibleBounds);
+  }
+  else
+  {
+    minimumVisible = CGRectGetMinY(visibleBounds);
+    maximumVisible = CGRectGetMaxY(visibleBounds);
+  }
+  [self updateVisibleAreaWithMinimumEdge:minimumVisible maximumEdge:maximumVisible];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Updates the visible area in the range between @a minimumEdge and
+/// @a maximumEdge.
+///
+/// This method is usually invoked because of scrolling. This method checks if
+/// new item views have become visible in the visible area. If not, then nothing
+/// happens. If yes, the following two-step process is initiated:
+/// - New item view are requested from the data source and added as subviews to
+///   the container view so that they become visible at either the minimum or
+///   the maximum edge (depending on the direction of scrolling)
+/// - Item views that are no longer visible are removed from the container view
+///   so that they can be deallocated if no one else keeps a reference to them.
+///
+/// The orientation of the ItemScrollView decides whether the visible area
+/// extends on the X- or Y-axis. If the orientation is horizontal, the
+/// minimum/maximum edges are on the left/right. If the orientation is vertical,
+/// the minimum/maximum edges are at the top/bottom.
+// -----------------------------------------------------------------------------
+- (void) updateVisibleAreaWithMinimumEdge:(CGFloat)minimumEdge maximumEdge:(CGFloat)maximumEdge
+{
+  // To make the implementation of subsequent steps easier we need to make sure
+  // that the array already contains at least one item view
+  if (visibleItems.count == 0)
+  {
+    UIView* itemView = [self firstItemView];
+    if (nil == itemView)  // check if data source does not provide any items
+      return;
+    [self placeItemView:itemView withMinimumEdgeAt:minimumEdge];
+  }
+
+  [self updateVisibleAreaAtMaximumEdge:maximumEdge];
+  [self updateVisibleAreaFromMinimumEdge:minimumEdge];
+
+  [self removeItemsAfterMaximumEdge:maximumEdge];
+  [self removeItemsBeforeMinimumEdge:minimumEdge];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaWithMinimumEdge:maximumEdge:()
+// -----------------------------------------------------------------------------
+- (void) updateVisibleAreaAtMaximumEdge:(CGFloat)maximumVisible
+{
+  NSArray* lastItemArray = [visibleItems lastObject];
+  UIView* lastItemView = [lastItemArray objectAtIndex:0];
+  CGFloat maximumEdge;
+  if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+    maximumEdge = CGRectGetMaxX(lastItemView.frame);
+  else
+    maximumEdge = CGRectGetMaxY(lastItemView.frame);
+  while (maximumEdge < maximumVisible)
+  {
+    UIView* nextItemView = [self nextItemView];
+    if (nil == nextItemView)  // check for bouncing
+      break;
+    maximumEdge = [self placeItemView:nextItemView withMinimumEdgeAt:maximumEdge];
+  }
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaAtMaximumEdge:()
+// -----------------------------------------------------------------------------
+- (UIView*) nextItemView
+{
+  NSArray* lastItemArray = [visibleItems lastObject];
+  NSNumber* lastItemIndex = [lastItemArray lastObject];
+  int indexOfNewItem = [lastItemIndex intValue] + 1;
+  if (indexOfNewItem >= numberOfItemsInItemScrollView)  // check for bouncing
+    return nil;
+  
+  UIView* itemView = [itemScrollViewDataSource itemScrollView:self itemViewAtIndex:indexOfNewItem];
+  [itemContainerView addSubview:itemView];
+  
+  NSArray* newLastItemArray = [NSArray arrayWithObjects:itemView, [NSNumber numberWithInt:indexOfNewItem], nil];
+  [visibleItems addObject:newLastItemArray];
+  
+  return itemView;
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaAtMaximumEdge:()
+// -----------------------------------------------------------------------------
+- (CGFloat) placeItemView:(UIView*)itemView withMinimumEdgeAt:(CGFloat)position
+{
+  CGRect newItemViewFrame = itemView.frame;
+  CGFloat newMinimumEdge;
+  if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+  {
+    newItemViewFrame.origin.x = position;
+    newItemViewFrame.origin.y = 0;
+    newMinimumEdge = CGRectGetMaxX(newItemViewFrame);
+  }
+  else
+  {
+    newItemViewFrame.origin.x = 0;
+    newItemViewFrame.origin.y = position;
+    newMinimumEdge = CGRectGetMaxY(newItemViewFrame);
+  }
+  itemView.frame = newItemViewFrame;
+
+  if (itemScrollViewDelegate)
+    [itemScrollViewDelegate itemScrollView:self willDisplayItemView:itemView];
+
+  return newMinimumEdge;
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaWithMinimumEdge:maximumEdge:()
+// -----------------------------------------------------------------------------
+- (void) updateVisibleAreaFromMinimumEdge:(CGFloat)minimumVisible
+{
+  NSArray* firstItemArray = [visibleItems objectAtIndex:0];
+  UIView* firstItemView = [firstItemArray objectAtIndex:0];
+  CGFloat minimumEdge;
+  if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+    minimumEdge = CGRectGetMinX(firstItemView.frame);
+  else
+    minimumEdge = CGRectGetMinY(firstItemView.frame);
+  while (minimumEdge > minimumVisible)
+  {
+    UIView* previousItemView = [self previousItemView];
+    if (nil == previousItemView)  // check for bouncing
+      break;
+    minimumEdge = [self placeItemView:previousItemView withMaximumEdgeAt:minimumEdge];
+  }
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaFromMinimumEdge:()
+// -----------------------------------------------------------------------------
+- (UIView*) previousItemView
+{
+  NSArray* firstItemArray = [visibleItems objectAtIndex:0];
+  NSNumber* firstItemIndex = [firstItemArray lastObject];
+  int indexOfNewItem = [firstItemIndex intValue] - 1;
+  if (indexOfNewItem < 0)  // check for bouncing
+    return nil;
+
+  UIView* itemView = [itemScrollViewDataSource itemScrollView:self itemViewAtIndex:indexOfNewItem];
+  [itemContainerView addSubview:itemView];
+
+  NSArray* newFirstItemArray = [NSArray arrayWithObjects:itemView, [NSNumber numberWithInt:indexOfNewItem], nil];
+  [visibleItems insertObject:newFirstItemArray atIndex:0];
+
+  return itemView;
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaFromMinimumEdge:()
+// -----------------------------------------------------------------------------
+- (CGFloat) placeItemView:(UIView*)itemView withMaximumEdgeAt:(CGFloat)position
+{
+  CGRect newItemViewFrame = itemView.frame;
+  CGFloat newMaximumEdge;
+  if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+  {
+    newItemViewFrame.origin.x = position - newItemViewFrame.size.width;
+    newItemViewFrame.origin.y = 0;
+    newMaximumEdge = CGRectGetMinX(newItemViewFrame);
+  }
+  else
+  {
+    newItemViewFrame.origin.x = 0;
+    newItemViewFrame.origin.y = position - newItemViewFrame.size.height;
+    newMaximumEdge = CGRectGetMinY(newItemViewFrame);
+  }
+  itemView.frame = newItemViewFrame;
+
+  if (itemScrollViewDelegate)
+    [itemScrollViewDelegate itemScrollView:self willDisplayItemView:itemView];
+
+  return newMaximumEdge;
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaWithMinimumEdge:maximumEdge:()
+// -----------------------------------------------------------------------------
+- (void) removeItemsAfterMaximumEdge:(CGFloat)maximumVisible
+{
+  NSArray* lastItemArray = [visibleItems lastObject];
+  UIView* lastItemView = [lastItemArray objectAtIndex:0];
+  while (true)
+  {
+    if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+    {
+      if (lastItemView.frame.origin.x <= maximumVisible)
+        break;
+    }
+    else
+    {
+      if (lastItemView.frame.origin.y <= maximumVisible)
+        break;
+    }
+    [lastItemView removeFromSuperview];
+    [visibleItems removeLastObject];
+    lastItemArray = [visibleItems lastObject];
+    lastItemView = [lastItemArray objectAtIndex:0];
+  }
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaWithMinimumEdge:maximumEdge:()
+// -----------------------------------------------------------------------------
+- (void) removeItemsBeforeMinimumEdge:(CGFloat)minimumVisible
+{
+  NSArray* firstItemArray = [visibleItems objectAtIndex:0];
+  UIView* firstItemView = [firstItemArray objectAtIndex:0];
+  while (true)
+  {
+    if (ItemScrollViewOrientationHorizontal == itemScrollViewOrientation)
+    {
+      if (CGRectGetMaxX(firstItemView.frame) >= minimumVisible)
+        break;
+    }
+    else
+    {
+      if (CGRectGetMaxY(firstItemView.frame) >= minimumVisible)
+        break;
+    }
+    [firstItemView removeFromSuperview];
+    [visibleItems removeObjectAtIndex:0];
+    firstItemArray = [visibleItems objectAtIndex:0];
+    firstItemView = [firstItemArray objectAtIndex:0];
+  }
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Internal helper for updateVisibleAreaWithMinimumEdge:maximumEdge:()
+// -----------------------------------------------------------------------------
+- (UIView*) firstItemView
+{
+  if (0 == numberOfItemsInItemScrollView)
+    return nil;
+
+  int indexOfItem = 0;
+  UIView* itemView = [itemScrollViewDataSource itemScrollView:self itemViewAtIndex:indexOfItem];
+  [itemContainerView addSubview:itemView];
+
+  NSArray* itemArray = [NSArray arrayWithObjects:itemView, [NSNumber numberWithInt:indexOfItem], nil];
+  [visibleItems addObject:itemArray];
+
+  return itemView;
+}
+
+@end
