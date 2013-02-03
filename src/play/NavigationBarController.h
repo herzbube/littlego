herzbube/littlego

commit dd0e884c694614f858452be9d3adbfac2184d4c1

iPad layout: left pane of split view can now be displayed in portrait mode
change details
- PanGestureController now uses a delay for detecting its "long press" gesture;
  this fix allows a few touch events to get through to UISplitViewController so that
  it can react to a swipe gesture
- NavigationBarController is now the delegate for UISplitViewController, which
  allows it to display a button in the navigation bar for presenting the left pane of
  the split view
- Refactoring in PlayViewController due to timing issues (NavigationBarController
  must now exist before the UISplitViewController is created)

src/play/NavigationBarController.h
@@ -55,13 +55,17 @@
 /// - Enable/disable buttons
 /// - Reacting to the user tapping on buttons
 // -----------------------------------------------------------------------------
-@interface NavigationBarController : NSObject <GameInfoViewControllerDelegate, PlayViewActionSheetDelegate, UIAlertViewDelegate>
+@interface NavigationBarController : NSObject <GameInfoViewControllerDelegate,
+                                               PlayViewActionSheetDelegate,
+                                               UIAlertViewDelegate,
+                                               UISplitViewControllerDelegate>
 {
 }
 
-- (id) initWithNavigationBar:(UINavigationBar*)navigationBar
-          scoringModel:(ScoringModel*)scoringModel
+- (id) initWithScoringModel:(ScoringModel*)scoringModel
               delegate:(id<NavigationBarControllerDelegate>)delegate
   parentViewController:(UIViewController*)parentViewController;
+
+@property(nonatomic, retain) UINavigationBar* navigationBar;
 
 @end

src/play/NavigationBarController.m
@@ -55,6 +55,11 @@
 //@{
 - (void) playViewActionSheetControllerDidFinish:(PlayViewActionSheetController*)controller;
 //@}
+/// @name UISplitViewControllerDelegate protocol
+//@{
+- (void) splitViewController:(UISplitViewController*)svc willHideViewController:(UIViewController*)aViewController withBarButtonItem:(UIBarButtonItem*)barButtonItem forPopoverController:(UIPopoverController*)pc;
+- (void) splitViewController:(UISplitViewController*)svc willShowViewController:(UIViewController*)aViewController invalidatingBarButtonItem:(UIBarButtonItem*)button;
+//@}
 /// @name Notification responders
 //@{
 - (void) goGameWillCreate:(NSNotification*)notification;
@@ -93,7 +98,6 @@
 //@}
 /// @name Privately declared properties
 //@{
-@property(nonatomic, retain) UINavigationBar* navigationBar;
 @property(nonatomic, assign) ScoringModel* scoringModel;
 @property(nonatomic, assign) id<NavigationBarControllerDelegate> delegate;
 /// @brief The parent view controller of this subcontroller.
@@ -115,6 +119,7 @@
 @property(nonatomic, retain) UIBarButtonItem* continueButton;
 @property(nonatomic, retain) UIBarButtonItem* interruptButton;
 @property(nonatomic, retain) UIBarButtonItem* flexibleSpaceButton;
+@property(nonatomic, assign) UIBarButtonItem* barButtonItemForShowingTheHiddenViewController;
 @property(nonatomic, retain) UIBarButtonItem* gameInfoButton;
 @property(nonatomic, retain) UIBarButtonItem* gameActionsButton;
 @property(nonatomic, retain) UIBarButtonItem* doneButton;
@@ -141,6 +146,7 @@
 @synthesize continueButton;
 @synthesize interruptButton;
 @synthesize flexibleSpaceButton;
+@synthesize barButtonItemForShowingTheHiddenViewController;
 @synthesize gameInfoButton;
 @synthesize gameActionsButton;
 @synthesize doneButton;
@@ -151,8 +157,7 @@
 ///
 /// @note This is the designated initializer of NavigationBarController.
 // -----------------------------------------------------------------------------
-- (id) initWithNavigationBar:(UINavigationBar*)aNavigationBar
-          scoringModel:(ScoringModel*)aScoringModel
+- (id) initWithScoringModel:(ScoringModel*)aScoringModel
               delegate:(id<NavigationBarControllerDelegate>)aDelegate
   parentViewController:(UIViewController*)aParentViewController
 {
@@ -161,13 +166,14 @@
   if (! self)
     return nil;
 
-  self.navigationBar = aNavigationBar;
+  self.navigationBar = nil;
   self.scoringModel = aScoringModel;
   self.delegate = aDelegate;
   self.parentViewController = aParentViewController;
   self.gameInfoViewController = nil;
   self.gameInfoScore = nil;
   self.actionsInProgress = 0;
+  self.barButtonItemForShowingTheHiddenViewController = nil;
   [self setupNavigationItem];
   [self setupButtons];
   [self setupNotificationResponders];
@@ -202,6 +208,7 @@
   self.continueButton = nil;
   self.interruptButton = nil;
   self.flexibleSpaceButton = nil;
+  self.barButtonItemForShowingTheHiddenViewController = nil;
   self.gameInfoButton = nil;
   self.gameActionsButton = nil;
   self.doneButton = nil;
@@ -214,7 +221,6 @@
 - (void) setupNavigationItem
 {
   self.navigationItem = [[[UINavigationItem alloc] initWithTitle:@""] autorelease];
-  [self.navigationBar pushNavigationItem:self.navigationItem animated:NO];
 }
 
 // -----------------------------------------------------------------------------
@@ -284,6 +290,17 @@
   GoBoardPosition* boardPosition = [GoGame sharedGame].boardPosition;
   [boardPosition addObserver:self forKeyPath:@"currentBoardPosition" options:0 context:NULL];
   [boardPosition addObserver:self forKeyPath:@"numberOfBoardPositions" options:0 context:NULL];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Setting of navigation bar occurs delayed (i.e. not during
+/// initialization of the controller object) due to timing needs of the parent
+/// view controller.
+// -----------------------------------------------------------------------------
+- (void) setNavigationBar:(UINavigationBar*)aNavigationBar
+{
+  navigationBar = aNavigationBar;
+  [self.navigationBar pushNavigationItem:self.navigationItem animated:NO];
 }
 
 // -----------------------------------------------------------------------------
@@ -406,6 +423,27 @@
 - (void) playViewActionSheetControllerDidFinish:(PlayViewActionSheetController*)controller
 {
   [controller release];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief UISplitViewControllerDelegate protocol method.
+// -----------------------------------------------------------------------------
+- (void) splitViewController:(UISplitViewController*)svc willHideViewController:(UIViewController*)aViewController withBarButtonItem:(UIBarButtonItem*)barButtonItem forPopoverController:(UIPopoverController*)pc
+{
+  self.barButtonItemForShowingTheHiddenViewController = barButtonItem;
+  barButtonItem.title = @"Moves";
+  self.navigationBarNeedsPopulation = true;
+  [self delayedUpdate];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief UISplitViewControllerDelegate protocol method.
+// -----------------------------------------------------------------------------
+- (void) splitViewController:(UISplitViewController*)svc willShowViewController:(UIViewController*)aViewController invalidatingBarButtonItem:(UIBarButtonItem*)button
+{
+  self.barButtonItemForShowingTheHiddenViewController = nil;
+  self.navigationBarNeedsPopulation = true;
+  [self delayedUpdate];
 }
 
 // -----------------------------------------------------------------------------
@@ -639,6 +677,8 @@
   NSMutableArray* rightBarButtonItems = [NSMutableArray arrayWithCapacity:0];
   [rightBarButtonItems addObject:self.gameActionsButton];
   [rightBarButtonItems addObject:self.gameInfoButton];
+  if (self.barButtonItemForShowingTheHiddenViewController)
+    [rightBarButtonItems addObject:self.barButtonItemForShowingTheHiddenViewController];
   self.navigationItem.rightBarButtonItems = rightBarButtonItems;
 }
 

src/play/PlayViewController.m
@@ -125,6 +125,11 @@ enum ActionType
 - (UIView*) boardPositionListContainerViewSuperview;
 - (void) setupDebugView;
 //@}
+/// @name View controller setup
+//@{
+- (void) setupNavigationBarController;
+- (void) setupSplitViewController;
+//@}
 /// @name Private helpers
 //@{
 - (void) releaseObjects;
@@ -235,6 +240,24 @@ enum ActionType
   // time makeControllerReadyForAction() is invoked.
   [self setupMainView];
 
+  // Prerequisite for setupSplitViewController(). For the iPhone we could
+  // invoke this in makeControllerReadyForAction().
+  [self setupNavigationBarController];
+
+  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+  {
+    // Cannot delay creation of UISplitViewControlller until
+    // makeControllerReadyForAction() is invoked, otherwise swipe gestures
+    // are initially not recognized when the app is launched in portrait mode.
+    // UISplitViewControlller starts to recgonize swipe gesture if the device
+    // is rotated to landscape and back to portrait, but not before. The reason
+    // for this behaviour of UISplitViewControlller is unknown, but the only
+    // way I have found to fix the problem is to not delay creation of the
+    // controller.
+    [self setupSplitViewController];
+    [self setupSplitView];
+  }
+
   // Setup of remaining views is delayed to makeControllerReadyForAction()
 }
 
@@ -276,20 +299,6 @@ enum ActionType
 // -----------------------------------------------------------------------------
 - (void) setupSplitView
 {
-  self.splitViewController = [[[UISplitViewController alloc] init] autorelease];
-  self.leftPaneViewController = [[[LeftPaneViewController alloc] init] autorelease];
-  self.rightPaneViewController = [[[RightPaneViewController alloc] init] autorelease];
-  // Assign view controllers before first use of self.splitViewController.view
-  // (if we do it the other way round, a message is printed in the debug area in
-  // Xcode that warns us about the mistake; we should heed this warning,
-  // although no bad things [tm] could actually be observed)
-  self.splitViewController.viewControllers = [NSArray arrayWithObjects:self.leftPaneViewController, self.rightPaneViewController, nil];
-  // The following statement is essential so that the split view controller
-  // properly receives rotation events in iOS 5. In iOS6 rotation seems to work
-  // even if the statement is missing. Behaviour of iOS 5 was tested in
-  // simulator only.
-  [self addChildViewController:self.splitViewController];
-
   UIView* superView = [self splitViewSuperview];
   CGRect splitViewControllerViewFrame = [self splitViewFrame];
   self.splitViewController.view.frame = splitViewControllerViewFrame;
@@ -724,7 +733,6 @@ enum ActionType
   }
   else
   {
-    [self setupSplitView];
     [self setupNavigationBarMain];
     [self setupToolbarBoardPositionNavigation];
     [self setupPlayView];
@@ -750,10 +758,7 @@ enum ActionType
     assert(0);
   }
 
-  self.navigationBarController = [[[NavigationBarController alloc] initWithNavigationBar:self.navigationBarMain
-                                                                            scoringModel:scoringModel
-                                                                                delegate:self
-                                                                    parentViewController:self] autorelease];
+  self.navigationBarController.navigationBar = self.navigationBarMain;
   self.statusLineController = [StatusLineController controllerWithStatusLine:self.statusLine];
   self.activityIndicatorController = [ActivityIndicatorController controllerWithActivityIndicator:self.activityIndicator];
 
@@ -779,8 +784,48 @@ enum ActionType
   {
     self.boardPositionTableListViewController = [[[BoardPositionTableListViewController alloc] initWithContainerView:self.boardPositionListContainerView] autorelease];
   }
-  self.panGestureController = [[[PanGestureController alloc] initWithPlayView:self.playView scoringModel:scoringModel delegate:self] autorelease];
+  self.panGestureController = [[[PanGestureController alloc] initWithPlayView:self.playView
+                                                                 scoringModel:scoringModel
+                                                                     delegate:self
+                                                         parentViewController:self] autorelease];
   self.tapGestureController = [[[TapGestureController alloc] initWithPlayView:self.playView scoringModel:scoringModel] autorelease];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief This is an internal helper invoked at the appropriate time to setup
+/// the controller that manages the main navigation bar.
+// -----------------------------------------------------------------------------
+- (void) setupNavigationBarController
+{
+  ScoringModel* scoringModel = [ApplicationDelegate sharedDelegate].scoringModel;
+  self.navigationBarController = [[[NavigationBarController alloc] initWithScoringModel:scoringModel
+                                                                               delegate:self
+                                                                   parentViewController:self] autorelease];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief This is an internal helper invoked at the appropriate time to setup
+/// the split view controller used by the iPad layout.
+// -----------------------------------------------------------------------------
+- (void) setupSplitViewController
+{
+  self.splitViewController = [[[UISplitViewController alloc] init] autorelease];
+  self.leftPaneViewController = [[[LeftPaneViewController alloc] init] autorelease];
+  self.rightPaneViewController = [[[RightPaneViewController alloc] init] autorelease];
+  // Assign view controllers before first use of self.splitViewController.view
+  // (if we do it the other way round, a message is printed in the debug area in
+  // Xcode that warns us about the mistake; we should heed this warning,
+  // although no bad things [tm] could actually be observed)
+  self.splitViewController.viewControllers = [NSArray arrayWithObjects:self.leftPaneViewController, self.rightPaneViewController, nil];
+  // The following statement is essential so that the split view controller
+  // properly receives rotation events in iOS 5. In iOS6 rotation seems to work
+  // even if the statement is missing. Behaviour of iOS 5 was tested in
+  // simulator only.
+  [self addChildViewController:self.splitViewController];
+  // Must assign a delegate, otherwise UISplitViewController will not react to
+  // swipe gestures (tested in 5.1 and 6.0 simulator; 5.0 does not support the
+  // swipe anyway). Reported to Apple with problem ID 13133575.
+  self.splitViewController.delegate = self.navigationBarController;
 }
 
 // -----------------------------------------------------------------------------

src/main/Constants.h
@@ -34,6 +34,10 @@ extern const int crossHairPointDistanceFromFingerOnSmallestBoard;
 /// This is based on
 /// http://stackoverflow.com/questions/5905608/how-do-i-make-a-uitableviewcell-appear-disabled
 extern const float gDisabledViewAlpha;
+/// @brief On the iPad and in portrait orientation, the gesture recognizer on
+/// the PlayView must use a small delay so as not to interfere with the swipe
+/// gesture of the main UISplitViewController.
+extern const CFTimeInterval gPlayViewPanningDelayIPadPortrait;
 //@}
 
 // -----------------------------------------------------------------------------

src/main/Constants.m
@@ -19,6 +19,7 @@
 const float gHalfPixel = 0.5;
 const int crossHairPointDistanceFromFingerOnSmallestBoard = 2;
 const float gDisabledViewAlpha = 0.439216f;
+const CFTimeInterval gPlayViewPanningDelayIPadPortrait = 0.1;
 
 // Go constants
 const enum GoGameType gDefaultGameType = GoGameTypeComputerVsHuman;

src/play/gesture/PanGestureController.h
@@ -37,11 +37,23 @@
 // -----------------------------------------------------------------------------
 /// @brief The PanGestureController class is responsible for managing the pan
 /// gesture on the "Play" view. Panning is used to place a stone on the board.
+///
+/// Despite its name, PanGestureController does not use UIPanGestureRecognizer
+/// for gesture recognition, because UIPanGestureRecognizer requires a
+/// fingertip to travel a certain distance before the gesture is recognized as
+/// a pan.
+///
+/// PanGestureController uses UILongPressGestureRecognizer so that a stone can
+/// be displayed immediately when a fingertip touches the board (or after only
+/// a very short delay).
 // -----------------------------------------------------------------------------
 @interface PanGestureController : NSObject <UIGestureRecognizerDelegate>
 {
 }
 
-- (id) initWithPlayView:(PlayView*)playView scoringModel:(ScoringModel*)scoringModel delegate:(id<PanGestureControllerDelegate>)delegate;
+- (id) initWithPlayView:(PlayView*)playView
+           scoringModel:(ScoringModel*)scoringModel
+               delegate:(id<PanGestureControllerDelegate>)delegate
+   parentViewController:(UIViewController*)parentViewController;
 
 @end

src/play/gesture/PanGestureController.m
@@ -42,6 +42,7 @@
 //@}
 /// @name Notification responders
 //@{
+- (void) deviceOrientationDidChange:(NSNotification*)notification;
 - (void) goGameWillCreate:(NSNotification*)notification;
 - (void) goGameDidCreate:(NSNotification*)notification;
 - (void) goGameStateChanged:(NSNotification*)notification;
@@ -53,6 +54,7 @@
 /// @name Updaters
 //@{
 - (void) updatePanningEnabled;
+- (void) updateMinimumPressDuration;
 //@}
 /// @name Private helpers
 //@{
@@ -61,17 +63,11 @@
 //@}
 /// @name Privately declared properties
 //@{
-/// @brief The view that PanGestureController manages gestures for.
 @property(nonatomic, assign) PlayView* playView;
-/// @brief The model that manages scoring-related data.
 @property(nonatomic, assign) ScoringModel* scoringModel;
-/// @brief The delegate that is informed when the user attempts to place a
-/// stone via panning.
 @property(nonatomic, assign) id<PanGestureControllerDelegate> delegate;
-/// @brief The gesture recognizer used to detect the long-press gesture.
+@property(nonatomic, assign) UIViewController* parentViewController;
 @property(nonatomic, retain) UILongPressGestureRecognizer* longPressRecognizer;
-/// @brief True if a panning gesture is currently allowed, false if not (e.g.
-/// while a computer player is thinking).
 @property(nonatomic, assign, getter=isPanningEnabled) bool panningEnabled;
 @end
 
@@ -81,6 +77,7 @@
 @synthesize playView;
 @synthesize scoringModel;
 @synthesize delegate;
+@synthesize parentViewController;
 @synthesize longPressRecognizer;
 @synthesize panningEnabled;
 
@@ -90,7 +87,10 @@
 ///
 /// @note This is the designated initializer of PanGestureController.
 // -----------------------------------------------------------------------------
-- (id) initWithPlayView:(PlayView*)aPlayView scoringModel:(ScoringModel*)aScoringModel delegate:(id<PanGestureControllerDelegate>)aDelegate
+- (id) initWithPlayView:(PlayView*)aPlayView
+           scoringModel:(ScoringModel*)aScoringModel
+               delegate:(id<PanGestureControllerDelegate>)aDelegate
+   parentViewController:(UIViewController*)aParentViewController
 {
   // Call designated initializer of superclass (NSObject)
   self = [super init];
@@ -100,10 +100,12 @@
   self.playView = aPlayView;
   self.scoringModel = aScoringModel;
   self.delegate = aDelegate;
+  self.parentViewController = aParentViewController;
 
   [self setupLongPressGestureRecognizer];
   [self setupNotificationResponders];
   [self updatePanningEnabled];
+  [self updateMinimumPressDuration];
 
   return self;
 }
@@ -131,7 +133,6 @@
 	[self.longPressRecognizer release];
 	[self.playView addGestureRecognizer:self.longPressRecognizer];
   self.longPressRecognizer.delegate = self;
-  self.longPressRecognizer.minimumPressDuration = 0;  // place stone immediately
   CGFloat infiniteMovement = CGFLOAT_MAX;
   self.longPressRecognizer.allowableMovement = infiniteMovement;  // let the user pan as long as he wants
 }
@@ -142,6 +143,7 @@
 - (void) setupNotificationResponders
 {
   NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+  [center addObserver:self selector:@selector(deviceOrientationDidChange:) name:UIDeviceOrientationDidChangeNotification object:nil];
   [center addObserver:self selector:@selector(goGameWillCreate:) name:goGameWillCreate object:nil];
   [center addObserver:self selector:@selector(goGameDidCreate:) name:goGameDidCreate object:nil];
   [center addObserver:self selector:@selector(goGameStateChanged:) name:goGameStateChanged object:nil];
@@ -235,6 +237,15 @@
 - (BOOL) gestureRecognizerShouldBegin:(UIGestureRecognizer*)gestureRecognizer
 {
   return (self.isPanningEnabled ? YES : NO);
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Responds to the UIDeviceOrientationDidChangeNotification
+/// notification.
+// -----------------------------------------------------------------------------
+- (void) deviceOrientationDidChange:(NSNotification*)notification
+{
+  [self updateMinimumPressDuration];
 }
 
 // -----------------------------------------------------------------------------
@@ -343,6 +354,23 @@
       self.panningEnabled = false;
       break;
   }
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Updates the delay after which the panning gesture is recognized.
+// -----------------------------------------------------------------------------
+- (void) updateMinimumPressDuration
+{
+  CFTimeInterval minimumPressDuration = 0;
+  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+  {
+    // Can't use the UIDevice object's orientation property, at application
+    // launch this returns UIDeviceOrientationUnknown :-(
+    bool isPortraitOrientation = UIInterfaceOrientationIsPortrait(self.parentViewController.interfaceOrientation);
+    if (isPortraitOrientation)
+      minimumPressDuration = gPlayViewPanningDelayIPadPortrait;
+  }
+  self.longPressRecognizer.minimumPressDuration = minimumPressDuration;
 }
 
 @end

commit db544d14746d0752f00dddbd6f6956d31734b918

fix bug introduced in dd0e884: ScoringModel not yet available
Since dd0e884 NavigationBarController is initialized at an early time during
application launch where the ScoringModel object does not exist yet. This
bugfix makes sure that NavigationBarController is configured with the model
object at a later time.

src/play/NavigationBarController.h
@@ -62,10 +62,9 @@
 {
 }
 
-- (id) initWithScoringModel:(ScoringModel*)scoringModel
-              delegate:(id<NavigationBarControllerDelegate>)delegate
-  parentViewController:(UIViewController*)parentViewController;
+- (id) initWithDelegate:(id<NavigationBarControllerDelegate>)delegate parentViewController:(UIViewController*)parentViewController;
 
+@property(nonatomic, assign) ScoringModel* scoringModel;
 @property(nonatomic, retain) UINavigationBar* navigationBar;
 
 @end

src/play/NavigationBarController.m
@@ -98,7 +98,6 @@
 //@}
 /// @name Privately declared properties
 //@{
-@property(nonatomic, assign) ScoringModel* scoringModel;
 @property(nonatomic, assign) id<NavigationBarControllerDelegate> delegate;
 /// @brief The parent view controller of this subcontroller.
 @property(nonatomic, assign) UIViewController* parentViewController;
@@ -134,17 +133,15 @@
 ///
 /// @note This is the designated initializer of NavigationBarController.
 // -----------------------------------------------------------------------------
-- (id) initWithScoringModel:(ScoringModel*)aScoringModel
-              delegate:(id<NavigationBarControllerDelegate>)aDelegate
-  parentViewController:(UIViewController*)aParentViewController
+- (id) initWithDelegate:(id<NavigationBarControllerDelegate>)aDelegate parentViewController:(UIViewController*)aParentViewController
 {
   // Call designated initializer of superclass (NSObject)
   self = [super init];
   if (! self)
     return nil;
 
-  self.navigationBar = nil;
-  self.scoringModel = aScoringModel;
+  _navigationBar = nil;  // don't use self to avoid invoking the setter
+  _scoringModel = nil;   // ditto
   self.delegate = aDelegate;
   self.parentViewController = aParentViewController;
   self.gameInfoViewController = nil;
@@ -278,6 +275,19 @@
 {
   _navigationBar = aNavigationBar;
   [_navigationBar pushNavigationItem:self.navigationItem animated:NO];
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Setting of navigation bar occurs delayed (i.e. not during
+/// initialization of the controller object) due to timing needs of the parent
+/// view controller.
+// -----------------------------------------------------------------------------
+- (void) setScoringModel:(ScoringModel*)scoringModel
+{
+  _scoringModel = scoringModel;
+  self.navigationBarNeedsPopulation = true;
+  self.buttonStatesNeedUpdate = true;
+  [self delayedUpdate];
 }
 
 // -----------------------------------------------------------------------------
@@ -608,8 +618,8 @@
   {
     if (GoGameStateGameHasEnded != game.state)
     {
-      [leftBarButtonItems addObject:self.discardBoardPositionButton];
       [leftBarButtonItems addObject:self.doneButton];  // cannot get out of scoring mode if game has ended
+      [leftBarButtonItems addObject:self.discardBoardPositionButton];
     }
   }
   else

src/play/PlayViewController.m
@@ -739,12 +739,8 @@ enum ActionType
 - (void) setupSubcontrollers
 {
   ScoringModel* scoringModel = [ApplicationDelegate sharedDelegate].scoringModel;
-  if (! scoringModel)
-  {
-    DDLogError(@"PlayViewController::makeControllerReadyForAction(): Unable to find the ScoringModel object");
-    assert(0);
-  }
 
+  self.navigationBarController.scoringModel = scoringModel;
   self.navigationBarController.navigationBar = self.navigationBarMain;
   self.statusLineController = [StatusLineController controllerWithStatusLine:self.statusLine];
   self.activityIndicatorController = [ActivityIndicatorController controllerWithActivityIndicator:self.activityIndicator];
@@ -786,10 +782,8 @@ enum ActionType
 // -----------------------------------------------------------------------------
 - (void) setupNavigationBarController
 {
-  ScoringModel* scoringModel = [ApplicationDelegate sharedDelegate].scoringModel;
-  self.navigationBarController = [[[NavigationBarController alloc] initWithScoringModel:scoringModel
-                                                                               delegate:self
-                                                                   parentViewController:self] autorelease];
+  self.navigationBarController = [[[NavigationBarController alloc] initWithDelegate:self
+                                                               parentViewController:self] autorelease];
 }
 
 // -----------------------------------------------------------------------------