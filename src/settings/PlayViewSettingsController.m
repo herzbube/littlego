herzbube/littlego

commit 432aa2076b4c25077a628861cffee813addcf4b6

add user preference "display move numbers"

src/settings/PlayViewSettingsController.m
@@ -25,6 +25,7 @@
 
 // Constants
 static const float sliderValueFactorForMaximumZoomScale = 10.0;
+static const float sliderValueFactorForMoveNumbersPercentage = 100.0;
 static const float sliderValueFactorForStoneDistanceFromFingertip = 5.0;
 
 
@@ -57,7 +58,7 @@ enum ViewSectionItem
 {
   MarkLastMoveItem,
 //  DisplayCoordinatesItem,
-//  DisplayMoveNumbersItem,
+  MoveNumbersPercentageItem,
   StoneDistanceFromFingertipItem,
   MaxViewSectionItem
 };
@@ -102,7 +103,6 @@ enum ZoomSectionItem
 - (void) toggleVibrate:(id)sender;
 - (void) toggleMarkLastMove:(id)sender;
 - (void) toggleDisplayCoordinates:(id)sender;
-- (void) toggleDisplayMoveNumbers:(id)sender;
 //@}
 /// @name Privately declared properties
 //@{
@@ -271,11 +271,20 @@ enum ZoomSectionItem
 //            accessoryView.on = self.playViewModel.displayCoordinates;
 //            [accessoryView addTarget:self action:@selector(toggleDisplayCoordinates:) forControlEvents:UIControlEventValueChanged];
 //            break;
-//          case DisplayMoveNumbersItem:
-//            cell.textLabel.text = @"Move numbers";
-//            accessoryView.on = self.playViewModel.displayMoveNumbers;
-//            [accessoryView addTarget:self action:@selector(toggleDisplayMoveNumbers:) forControlEvents:UIControlEventValueChanged];
-//            break;
+        case MoveNumbersPercentageItem:
+        {
+          cell = [TableViewCellFactory cellWithType:SliderCellType tableView:tableView];
+          TableViewSliderCell* sliderCell = (TableViewSliderCell*)cell;
+          sliderCell.valueLabelHidden = true;
+          [sliderCell setDelegate:self actionValueDidChange:nil actionSliderValueDidChange:@selector(moveNumbersPercentageDidChange:)];
+          sliderCell.descriptionLabel.text = @"Display move numbers";
+          sliderCell.slider.minimumValue = 0;
+          sliderCell.slider.maximumValue = (1.0
+                                            * sliderValueFactorForMoveNumbersPercentage);
+          sliderCell.value = (self.playViewModel.moveNumbersPercentage
+                              * sliderValueFactorForMoveNumbersPercentage);
+          break;
+        }
         case StoneDistanceFromFingertipItem:
         {
           cell = [TableViewCellFactory cellWithType:SliderCellType tableView:tableView];
@@ -341,10 +350,31 @@ enum ZoomSectionItem
 - (CGFloat) tableView:(UITableView*)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath
 {
   CGFloat height = tableView.rowHeight;
-  if ((ViewSection == indexPath.section && StoneDistanceFromFingertipItem == indexPath.row) ||
-      (ZoomSection == indexPath.section && MaxZoomScaleItem == indexPath.row))
+  switch (indexPath.section)
   {
-    height = [TableViewSliderCell rowHeightInTableView:tableView];
+    case ViewSection:
+    {
+      switch (indexPath.row)
+      {
+        case MoveNumbersPercentageItem:
+        case StoneDistanceFromFingertipItem:
+          height = [TableViewSliderCell rowHeightInTableView:tableView];
+          break;
+        default:
+          break;
+      }
+    }
+    case ZoomSection:
+    {
+      switch (indexPath.row)
+      {
+        case MaxZoomScaleItem:
+          height = [TableViewSliderCell rowHeightInTableView:tableView];
+          break;
+        default:
+          break;
+      }
+    }
   }
   return height;
 }
@@ -398,13 +428,12 @@ enum ZoomSectionItem
 }
 
 // -----------------------------------------------------------------------------
-/// @brief Reacts to a tap gesture on the "Display move numbers" switch. Writes
-/// the new value to the appropriate model.
+/// @brief Reacts to the user changing the "display move numbers" setting.
 // -----------------------------------------------------------------------------
-- (void) toggleDisplayMoveNumbers:(id)sender
+- (void) moveNumbersPercentageDidChange:(id)sender
 {
-  UISwitch* accessoryView = (UISwitch*)sender;
-  self.playViewModel.displayMoveNumbers = accessoryView.on;
+  TableViewSliderCell* sliderCell = (TableViewSliderCell*)sender;
+  self.playViewModel.moveNumbersPercentage = (1.0 * sliderCell.value / sliderValueFactorForMoveNumbersPercentage);
 }
 
 // -----------------------------------------------------------------------------

resource/plist/RegistrationDomainDefaults.plist
@@ -12,8 +12,8 @@
 		<true/>
 		<key>DisplayCoordinates</key>
 		<false/>
-		<key>DisplayMoveNumbers</key>
-		<false/>
+		<key>MoveNumbersPercentage</key>
+		<real>0</real>
 		<key>PlaySound</key>
 		<false/>
 		<key>Vibrate</key>

src/main/Constants.h
@@ -415,6 +415,7 @@ extern const float maximumZoomScaleDefault;
 extern const float maximumZoomScaleMaximum;
 extern const float stoneDistanceFromFingertipDefault;
 extern const float stoneDistanceFromFingertipMaximum;
+extern const float moveNumbersPercentageDefault;
 //@}
 
 // -----------------------------------------------------------------------------
@@ -576,7 +577,7 @@ extern NSString* userDefaultsVersionApplicationDomainKey;
 extern NSString* playViewKey;
 extern NSString* markLastMoveKey;
 extern NSString* displayCoordinatesKey;
-extern NSString* displayMoveNumbersKey;
+extern NSString* moveNumbersPercentageKey;
 extern NSString* playSoundKey;
 extern NSString* vibrateKey;
 extern NSString* backgroundColorKey;

src/main/Constants.m
@@ -72,6 +72,7 @@ const float maximumZoomScaleDefault = 3.0;
 const float maximumZoomScaleMaximum = 3.0;
 const float stoneDistanceFromFingertipDefault = 2.0;
 const float stoneDistanceFromFingertipMaximum = 4.0;
+const float moveNumbersPercentageDefault = 0.0;
 
 // Board position settings default values
 const bool discardFutureMovesAlertDefault = true;
@@ -181,7 +182,7 @@ NSString* userDefaultsVersionApplicationDomainKey = @"UserDefaultsVersionApplica
 NSString* playViewKey = @"PlayView";
 NSString* markLastMoveKey = @"MarkLastMove";
 NSString* displayCoordinatesKey = @"DisplayCoordinates";
-NSString* displayMoveNumbersKey = @"DisplayMoveNumbers";
+NSString* moveNumbersPercentageKey = @"MoveNumbersPercentage";
 NSString* playSoundKey = @"PlaySound";
 NSString* vibrateKey = @"Vibrate";
 NSString* backgroundColorKey = @"BackgroundColor";

src/play/PlayView.m
@@ -178,7 +178,7 @@ static PlayView* sharedPlayView = nil;
   [[NSNotificationCenter defaultCenter] removeObserver:self];
   [self.playViewModel removeObserver:self forKeyPath:@"markLastMove"];
   [self.playViewModel removeObserver:self forKeyPath:@"displayCoordinates;"];
-  [self.playViewModel removeObserver:self forKeyPath:@"displayMoveNumbers"];
+  [self.playViewModel removeObserver:self forKeyPath:@"moveNumbersPercentage"];
   [self.playViewModel removeObserver:self forKeyPath:@"stoneDistanceFromFingertip"];
   [self.scoringModel removeObserver:self forKeyPath:@"inconsistentTerritoryMarkupType"];
   [[GoGame sharedGame].boardPosition removeObserver:self forKeyPath:@"currentBoardPosition"];
@@ -242,7 +242,7 @@ static PlayView* sharedPlayView = nil;
   // KVO observing
   [self.playViewModel addObserver:self forKeyPath:@"markLastMove" options:0 context:NULL];
   [self.playViewModel addObserver:self forKeyPath:@"displayCoordinates;" options:0 context:NULL];
-  [self.playViewModel addObserver:self forKeyPath:@"displayMoveNumbers" options:0 context:NULL];
+  [self.playViewModel addObserver:self forKeyPath:@"moveNumbersPercentage" options:0 context:NULL];
   [self.playViewModel addObserver:self forKeyPath:@"stoneDistanceFromFingertip" options:0 context:NULL];
   [self.scoringModel addObserver:self forKeyPath:@"inconsistentTerritoryMarkupType" options:0 context:NULL];
   GoGame* game = [GoGame sharedGame];
@@ -504,9 +504,9 @@ static PlayView* sharedPlayView = nil;
       [self notifyLayerDelegates:PVLDEventDisplayCoordinatesChanged eventInfo:nil];
       [self delayedUpdate];
     }
-    else if ([keyPath isEqualToString:@"displayMoveNumbers"])
+    else if ([keyPath isEqualToString:@"moveNumbersPercentage"])
     {
-      [self notifyLayerDelegates:PVLDEventDisplayMoveNumbersChanged eventInfo:nil];
+      [self notifyLayerDelegates:PVLDEventMoveNumbersPercentageChanged eventInfo:nil];
       [self delayedUpdate];
     }
     else if ([keyPath isEqualToString:@"stoneDistanceFromFingertip"])

src/play/PlayViewModel.h
@@ -29,7 +29,7 @@
 
 @property(nonatomic, assign) bool markLastMove;
 @property(nonatomic, assign) bool displayCoordinates;
-@property(nonatomic, assign) bool displayMoveNumbers;
+@property(nonatomic, assign) float moveNumbersPercentage;
 @property(nonatomic, assign) bool playSound;
 @property(nonatomic, assign) bool vibrate;
 @property(nonatomic, retain) UIColor* backgroundColor;

src/play/PlayViewModel.m
@@ -48,7 +48,7 @@
 
   self.markLastMove = false;
   self.displayCoordinates = false;
-  self.displayMoveNumbers = false;
+  self.moveNumbersPercentage = 0.0;
   self.playSound = false;
   self.vibrate = false;
   self.backgroundColor = [UIColor whiteColor];
@@ -90,7 +90,7 @@
   NSDictionary* dictionary = [userDefaults dictionaryForKey:playViewKey];
   self.markLastMove = [[dictionary valueForKey:markLastMoveKey] boolValue];
   self.displayCoordinates = [[dictionary valueForKey:displayCoordinatesKey] boolValue];
-  self.displayMoveNumbers = [[dictionary valueForKey:displayMoveNumbersKey] boolValue];
+  self.moveNumbersPercentage = [[dictionary valueForKey:moveNumbersPercentageKey] floatValue];
   self.playSound = [[dictionary valueForKey:playSoundKey] boolValue];
   self.vibrate = [[dictionary valueForKey:vibrateKey] boolValue];
   self.backgroundColor = [UIColor colorFromHexString:[dictionary valueForKey:backgroundColorKey]];
@@ -125,7 +125,7 @@
   // Note: Use NSNumber to represent int and bool values as an object.
   [dictionary setValue:[NSNumber numberWithBool:self.markLastMove] forKey:markLastMoveKey];
   [dictionary setValue:[NSNumber numberWithBool:self.displayCoordinates] forKey:displayCoordinatesKey];
-  [dictionary setValue:[NSNumber numberWithBool:self.displayMoveNumbers] forKey:displayMoveNumbersKey];
+  [dictionary setValue:[NSNumber numberWithFloat:self.moveNumbersPercentage] forKey:moveNumbersPercentageKey];
   [dictionary setValue:[NSNumber numberWithBool:self.playSound] forKey:playSoundKey];
   [dictionary setValue:[NSNumber numberWithBool:self.vibrate] forKey:vibrateKey];
   [dictionary setValue:[UIColor hexStringFromUIColor:self.backgroundColor] forKey:backgroundColorKey];

src/play/layer/PlayViewLayerDelegate.h
@@ -29,7 +29,7 @@ enum PlayViewLayerDelegateEvent
   PVLDEventBoardPositionChanged,
   PVLDEventMarkLastMoveChanged,
   PVLDEventDisplayCoordinatesChanged,
-  PVLDEventDisplayMoveNumbersChanged,
+  PVLDEventMoveNumbersPercentageChanged,
   PVLDEventInconsistentTerritoryMarkupTypeChanged,
   /// @brief The event info object that accompanies this event type is a GoPoint
   /// object that identifies the location of the cross-hair center.

commit e1eceee92463b71082ede3484e518e1b8532c87c

fix row height of "mark last move" table view cell (bug introduced in 432aa20)

src/settings/PlayViewSettingsController.m
@@ -364,6 +364,7 @@ enum ZoomSectionItem
         default:
           break;
       }
+      break;
     }
     case ZoomSection:
     {
@@ -375,6 +376,11 @@ enum ZoomSectionItem
         default:
           break;
       }
+      break;
+    }
+    default:
+    {
+      break;
     }
   }
   return height;