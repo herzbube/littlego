herzbube/littlego

commit bc9cea9fb0acd81213eb483052f03789eec8b2c4

final touches to Inbox folder management
prevent Inbox folder from being displayed on the Archive tab; folder is
deleted on application launch; prevent user from saving a file with the
name "Inbox" (the last one is not strictly necessary since an .sgf extension
is tacked on to the file name)

src/main/ApplicationDelegate.mm
@@ -413,8 +413,9 @@ static ApplicationDelegate* sharedDelegate = nil;
 }
 
 // -----------------------------------------------------------------------------
-/// @brief Sets up a number of folders in the application bundle, creating them
-/// if they do not exist.
+/// @brief Sets up a number of folders in the application bundle. Depending on
+/// the folder's purpose, it is either created if it does not exist, or deleted
+/// if it does exist.
 // -----------------------------------------------------------------------------
 - (void) setupFolders
 {
@@ -439,6 +440,15 @@ static ApplicationDelegate* sharedDelegate = nil;
            withIntermediateDirectories:YES
                             attributes:nil
                                  error:nil];
+  }
+  // Must not remove the Inbox folder if the application is launched because of
+  // document interaction. Removal in this case is delayed to the next app
+  // launch without document interaction.
+  if (! self.documentInteractionURL)
+  {
+    NSString* inboxDirectory = [documentsDirectory stringByAppendingPathComponent:inboxFolderName];
+    if ([fileManager fileExistsAtPath:inboxDirectory])
+      [fileManager removeItemAtPath:inboxDirectory error:nil];
   }
 }
 

src/archive/ArchiveViewModel.m
@@ -202,7 +202,7 @@
     return true;
   if ([fileName isEqualToString:bugReportDiagnosticsInformationFileName])
     return true;
-  if ([fileName isEqualToString:@"Inbox"])  // ignore folder where document interaction places file
+  if ([fileName isEqualToString:inboxFolderName])  // ignore folder where document interaction places file
     return true;
   return false;
 }

src/command/HandleDocumentInteraction.m
@@ -48,6 +48,11 @@
     return false;
   NSString* documentInteractionFilePath = [delegate.documentInteractionURL path];
   NSString* documentInteractionFileName = [documentInteractionFilePath lastPathComponent];
+  // The file always has an .sgf extension, iOS makes sure of that. Tested
+  // with a file downloaded via HTTP where the file has the proper MIME type,
+  // but a non-standard extension (i.e. something else than .sgf). When the user
+  // selects "Open in..." in Safari, the document interaction system passes the
+  // file into the app with the extension .sgf tacked on.
   NSString* preferredGameName = [documentInteractionFileName stringByDeletingPathExtension];
   ArchiveViewModel* model = delegate.archiveViewModel;
   NSString* uniqueGameName = [model uniqueGameNameForName:preferredGameName];

src/main/Constants.h
@@ -244,6 +244,9 @@ extern NSString* archiveBackupFileName;
 /// @brief Name of the secondary .sgf file used for the same purpose as
 /// @e archiveBackupFileName.
 extern NSString* sgfBackupFileName;
+/// @brief Name of the folder used by the document interaction system to pass
+/// files into the app. The folder is located in the Documents folder.
+extern NSString* inboxFolderName;
 //@}
 
 // -----------------------------------------------------------------------------

src/main/Constants.m
@@ -36,6 +36,7 @@ const double gDefaultKomi = 6.5;
 NSString* sgfTemporaryFileName = @"---tmp+++.sgf";
 NSString* archiveBackupFileName = @"backup.plist";
 NSString* sgfBackupFileName = @"backup.sgf";
+NSString* inboxFolderName = @"Inbox";
 
 // Application notifications
 NSString* applicationIsReadyForAction = @"ApplicationIsReadyForAction";

src/play/PlayViewActionSheetController.m
@@ -401,7 +401,8 @@ enum ActionSheetButton
     [alert release];
     return false;
   }
-  NSPredicate* predicateReservedWords = [NSPredicate predicateWithFormat:@"SELF MATCHES '^(.|..)$'"];
+  NSString* predicateFormatString = [NSString stringWithFormat:@"SELF MATCHES '^(.|..|%@)$'", inboxFolderName];
+  NSPredicate* predicateReservedWords = [NSPredicate predicateWithFormat:predicateFormatString];
   if ([predicateReservedWords evaluateWithObject:text])
   {
     UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Illegal game name"

commit 4f762bfbfa1abcdbec030c0dffc0115e6b0128e7

fix document interaction handling during app launch
the problem was a dependency bug introduced in commit bc9cea9: setupFolders depends
on self.documentInteractionURL being set, but document interaction handling occurred
too late. the fix is to perform document interaction handling earlier during app launch.

src/main/ApplicationDelegate.mm
@@ -73,6 +73,9 @@
 // -----------------------------------------------------------------------------
 @interface ApplicationDelegate()
 @property(nonatomic, retain) DDFileLogger* fileLogger;
+/// @brief Is true if application:OpenURL:sourceApplication:annotation: should
+/// not handle document interaction because
+/// application:didFinishLaunchingWithOptions: already does the handling.
 @property(nonatomic, assign) bool applicationOpenURLShouldIgnoreNextDocumentInteraction;
 @end
 
@@ -177,6 +180,7 @@ static ApplicationDelegate* sharedDelegate = nil;
   [self setupGUI];
   [self setupLogging];
   [self setupApplicationLaunchMode];
+  bool setupDocumentInteractionSuccess = [self setupDocumentInteraction:launchOptions];
   [self setupFolders];
   [self setupResourceBundle];
   [self setupRegistrationDomain];
@@ -186,30 +190,11 @@ static ApplicationDelegate* sharedDelegate = nil;
   [self setupTabBarController];
   [self setupPlayTab];
 
-  // If an URL is passed in, it is stored here and processed later during
-  // SetupApplicationCommand.
-  self.applicationOpenURLShouldIgnoreNextDocumentInteraction = false;
-  self.documentInteractionURL = nil;
-  BOOL canHandleURL = YES;
-  NSURL* url = (NSURL*)[launchOptions valueForKey:UIApplicationLaunchOptionsURLKey];
-  if (url)
-  {
-    if ([url isFileURL])
-    {
-      self.applicationOpenURLShouldIgnoreNextDocumentInteraction = true;
-      self.documentInteractionURL = url;
-    }
-    else
-    {
-      canHandleURL = NO;
-    }
-  }
-
-  // Further setup steps are executed in a secondary thread so that 1) we can
-  // display a progress HUD, and 2) the application launches as quickly as
-  // possible.
+  // Further setup steps are executed in a secondary thread so that we can
+  // display a progress HUD
   [[[[SetupApplicationCommand alloc] init] autorelease] submit];
 
+  BOOL canHandleURL = setupDocumentInteractionSuccess ? YES : NO;
   return canHandleURL;
 }
 
@@ -401,6 +386,41 @@ static ApplicationDelegate* sharedDelegate = nil;
     DDLogInfo(@"Launching in mode ApplicationLaunchModeNormal");
     self.applicationLaunchMode = ApplicationLaunchModeNormal;
   }
+}
+
+// -----------------------------------------------------------------------------
+/// @brief Scans @a launchOptions if it contains a document interaction URL,
+/// and if it does whether the URL can be handled.
+///
+/// Returns true for success, false for failure. Success means either
+/// - @a launchOptions does not contain an URL, or
+/// - @a launchOptions contains an URL that can be handled
+///
+/// Failure means that @a launchOptions contains an URL that cannot be handled.
+///
+/// As a side-effect, if a URL that can be handled is detected this method sets
+/// up document interaction related properties of this app delegate.
+// -----------------------------------------------------------------------------
+- (bool) setupDocumentInteraction:(NSDictionary*)launchOptions
+{
+  self.documentInteractionURL = nil;
+  self.applicationOpenURLShouldIgnoreNextDocumentInteraction = false;
+
+  bool success = true;
+  NSURL* url = [launchOptions valueForKey:UIApplicationLaunchOptionsURLKey];
+  if (url)
+  {
+    if ([url isFileURL])
+    {
+      self.documentInteractionURL = url;
+      self.applicationOpenURLShouldIgnoreNextDocumentInteraction = true;
+    }
+    else
+    {
+      success = false;
+    }
+  }
+  return success;
 }
 
 // -----------------------------------------------------------------------------