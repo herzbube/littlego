5a8fbc8c2e3a0bcc9cf0db62ef27003b6c3ac272
Patrick Näf <herzbube@herzbube.ch>
2016-06-22 16:19:10 +0200

    Cocoa Lumberjack is built (again) as static library
    This partially reverts the changes in commit
    2336b19fe3bd1a2f4830a2d0d8cf3589f999e7de. In that commit, Cocoa
    Lumberjack was updated to a new version. In the same commit this project
    was changed to use a framework build of Cocoa Lumberjack. As it turns
    out, that framework build produces a dynamic library that can't be used
    on iOS (on iOS all libraries must be statically linked).
    
    Here we revert back to building and using the static library version of
    Cocoa Lumberjack.

--- script/build-cocoalumberjack.sh
+++ script/build-cocoalumberjack.sh
@@ -14,21 +14,22 @@ SRC_DIR="$SRC_BASEDIR/CocoaLumberjack"
 
 # Variables describing the build
 LUMBERJACK_BUILD_CONFIGURATION="Release"
-LUMBERJACK_FRAMEWORK_NAME="CocoaLumberjack.framework"
+LUMBERJACK_BUILD_TARGET="CocoaLumberjack-iOS-Static"
+LUMBERJACK_BUILDRESULT_FILENAME="libCocoaLumberjack.a"
 
 # These paths are relative to the root directory of the extracted source archive
 LUMBERJACK_XCODEPROJ_FILENAME="Lumberjack.xcodeproj"
 LUMBERJACK_XCODEPROJ_BUILDDIR="build"
 LUMBERJACK_XCODEPROJ_IPHONEOS_BUILDDIR="$LUMBERJACK_XCODEPROJ_BUILDDIR/$LUMBERJACK_BUILD_CONFIGURATION-$IPHONEOS_SDKPREFIX"
 LUMBERJACK_XCODEPROJ_IPHONE_SIMULATOR_BUILDDIR="$LUMBERJACK_XCODEPROJ_BUILDDIR/$LUMBERJACK_BUILD_CONFIGURATION-$IPHONE_SIMULATOR_SDKPREFIX"
+LUMBERJACK_HEADER_SRCDIR="Classes"
 
-# Destination paths
-LUMBERJACK_FRAMEWORKS_DESTDIR="Frameworks"
-LUMBERJACK_IPHONEOS_DESTDIR="$IPHONEOS_PREFIXDIR/$LUMBERJACK_FRAMEWORKS_DESTDIR"
-LUMBERJACK_IPHONE_SIMULATOR_DESTDIR="$IPHONE_SIMULATOR_PREFIXDIR/$LUMBERJACK_FRAMEWORKS_DESTDIR"
+# These paths are relative to the destination PREFIXDIR
+LUMBERJACK_HEADER_DESTDIR="include/CocoaLumberjack"
+LUMBERJACK_LIB_DESTDIR="lib"
 
 # xcodebuild flags
-LUMBERJACK_COMMON_XCODEBUILDFLAGS="-configuration $LUMBERJACK_BUILD_CONFIGURATION -target CocoaLumberjack-iOS"
+LUMBERJACK_COMMON_XCODEBUILDFLAGS="-configuration $LUMBERJACK_BUILD_CONFIGURATION -target $LUMBERJACK_BUILD_TARGET"
 LUMBERJACK_IPHONEOS_XCODEBUILDFLAGS="-sdk $IPHONEOS_SDKNAME"
 LUMBERJACK_IPHONE_SIMULATOR_XCODEBUILDFLAGS="-sdk $IPHONE_SIMULATOR_SDKNAME"
 
@@ -98,6 +99,52 @@ BUILD_STEPS_SOFTWARE()
 }
 
 # +------------------------------------------------------------------------
+# | Performs steps to install the software for a specific platform.
+# |
+# | This function expects that the current working directory is the root
+# | directory of the extracted source archive.
+# +------------------------------------------------------------------------
+# | Arguments:
+# |  * Source BUILDDIR for the platform
+# |  * Destination PREFIXDIR for the platform
+# +------------------------------------------------------------------------
+# | Return values:
+# |  * 0: No error
+# |  * 1: Error
+# +------------------------------------------------------------------------
+INSTALL_STEPS_SOFTWARE_PLATFORM()
+{
+  typeset BUILDDIR="$1"
+  typeset PREFIXDIR="$2"
+
+  # Copy header files
+  rm -rf "$PREFIXDIR/$LUMBERJACK_HEADER_DESTDIR"
+  if test $? -ne 0; then
+    return 1
+  fi
+  mkdir -p "$PREFIXDIR/$LUMBERJACK_HEADER_DESTDIR"
+  if test $? -ne 0; then
+    return 1
+  fi
+  cp $LUMBERJACK_HEADER_SRCDIR/*.h "$PREFIXDIR/$LUMBERJACK_HEADER_DESTDIR"
+  if test $? -ne 0; then
+    return 1
+  fi
+
+  # Copy static library
+  if test ! -d "$PREFIXDIR/$LUMBERJACK_LIB_DESTDIR"; then
+    mkdir -p "$PREFIXDIR/$LUMBERJACK_LIB_DESTDIR"
+    if test $? -ne 0; then
+      return 1
+    fi
+  fi
+  cp "$BUILDDIR/$LUMBERJACK_BUILDRESULT_FILENAME" "$PREFIXDIR/$LUMBERJACK_LIB_DESTDIR"
+  if test $? -ne 0; then
+    return 1
+  fi
+}
+
+# +------------------------------------------------------------------------
 # | Performs steps to install the software.
 # |
 # | This function expects that the current working directory is the root
@@ -113,30 +160,14 @@ BUILD_STEPS_SOFTWARE()
 INSTALL_STEPS_SOFTWARE()
 {
   if test "$IPHONEOS_BUILD_ENABLED" = "1"; then
-    rm -rf "$LUMBERJACK_IPHONEOS_DESTDIR/$LUMBERJACK_FRAMEWORK_NAME"
-    if test $? -ne 0; then
-      return 1
-    fi
-    mkdir -p "$LUMBERJACK_IPHONEOS_DESTDIR"
-    if test $? -ne 0; then
-      return 1
-    fi
-    cp -R "$LUMBERJACK_XCODEPROJ_IPHONEOS_BUILDDIR/$LUMBERJACK_FRAMEWORK_NAME" "$LUMBERJACK_IPHONEOS_DESTDIR/$LUMBERJACK_FRAMEWORK_NAME"
+    INSTALL_STEPS_SOFTWARE_PLATFORM "$LUMBERJACK_XCODEPROJ_IPHONEOS_BUILDDIR" "$IPHONEOS_PREFIXDIR"
     if test $? -ne 0; then
       return 1
     fi
   fi
 
   if test "$IPHONE_SIMULATOR_BUILD_ENABLED" = "1"; then
-    rm -rf "$LUMBERJACK_IPHONE_SIMULATOR_DESTDIR/$LUMBERJACK_FRAMEWORK_NAME"
-    if test $? -ne 0; then
-      return 1
-    fi
-    mkdir -p "$LUMBERJACK_IPHONE_SIMULATOR_DESTDIR"
-    if test $? -ne 0; then
-      return 1
-    fi
-    cp -R "$LUMBERJACK_XCODEPROJ_IPHONE_SIMULATOR_BUILDDIR/$LUMBERJACK_FRAMEWORK_NAME" "$LUMBERJACK_IPHONE_SIMULATOR_DESTDIR/$LUMBERJACK_FRAMEWORK_NAME"
+    INSTALL_STEPS_SOFTWARE_PLATFORM "$LUMBERJACK_XCODEPROJ_IPHONE_SIMULATOR_BUILDDIR" "$IPHONE_SIMULATOR_PREFIXDIR"
     if test $? -ne 0; then
       return 1
     fi
