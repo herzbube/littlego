81dcb4a902639867122cfefcd419ff946a8ff994
Patrick Näf <herzbube@herzbube.ch>
2019-01-07 18:41:50 +0100

    disable Firebase Analytics data collection (#320)

--- src/main/ApplicationDelegate.mm
+++ src/main/ApplicationDelegate.mm
@@ -355,7 +355,21 @@ static std::streambuf* outputPipeStreamBuffer = nullptr;
 #ifndef LITTLEGO_UNITTESTS
   // TODO: Conditional compile this only when building for App Distribution
 
+  // Disable Firebase Analytics data collection. This can also be disabled via
+  // an Info.plist entry: FIREBASE_ANALYTICS_COLLECTION_DEACTIVATED = YES.
+  // For details see https://firebase.google.com/support/guides/disable-analytics
+  [[FIRAnalyticsConfiguration sharedInstance] setAnalyticsCollectionEnabled:NO];
+
+  // Another Firebase service, Analytics screen reporting, can only be disabled
+  // via an entry in Info.plist: FirebaseScreenReportingEnabled = NO. It seems
+  // to be impossible to disable this service programmatically.
+  //
+  // One way to disable everything programmatically is this:
+  //   [[FIRApp defaultApp] setDataCollectionDefaultEnabled:NO];
+  // Unfortunately this also disables crash reporting.
+
   [FIRApp configure];
+
 
   // Reading the bundle resource results in a string with a trailing newline,
   // character because the resource file also contains a newline. Fabric does
