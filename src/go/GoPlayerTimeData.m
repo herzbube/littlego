herzbube/littlego

commit f4f80eaa7ac16424b32b1e86be30a47163fe7972

add defensive check to GoPlayerTimeData (#29)

src/go/GoPlayerTimeData.m
@@ -553,7 +553,18 @@
   // Determine the effective time system after isRemainingTimeAbsoluteTime has
   // been updated
   GoTimeSystem* timeSystem = [self effectiveTimeSystem];
-  dataHasChanged |= [self performPeriodResetIfNecessary:timeSystem];
+
+  // There is no guarantee that SGF data is consistent. For instance, a
+  // GoNodeTimeData object with data that was loaded from SGF might refer to
+  // absolute time even though the SGF content did not define an absolute time
+  // system. Also, a GoNodeTimeData object might refer to the period-based
+  // time system, but it's a custom time system that the app does not
+  // understand. Unlike other updater methods, this one may be invoked even if
+  // such inconsistencies exist - because of that we have to check first if the
+  // time system actually supports timed play before we can perform a period
+  // reset.
+  if (! timeSystem.supportsTimedPlay)
+      dataHasChanged |= [self performPeriodResetIfNecessary:timeSystem];
 
   return dataHasChanged;
 }

commit 0df8fd714985c9a34dc45468161ad84999ddd605

fix period-reset bug introduced in commit f4f80eaa

src/go/GoPlayerTimeData.m
@@ -538,7 +538,8 @@
 
 // -----------------------------------------------------------------------------
 /// @brief Updates the time data in this GoPlayerTimeData object to be equal to
-/// the time data in @a nodeTimeData.
+/// the time data in @a nodeTimeData. Afterwards performs a period reset (if
+/// necessary).
 // -----------------------------------------------------------------------------
 - (bool) updateWithNodeTimeData:(GoNodeTimeData*)nodeTimeData
 {
@@ -585,7 +586,7 @@
   // such inconsistencies exist - because of that we have to check first if the
   // time system actually supports timed play before we can perform a period
   // reset.
-  if (! timeSystem.supportsTimedPlay)
+  if (timeSystem.supportsTimedPlay)
       dataHasChanged |= [self performPeriodResetIfNecessary:timeSystem];
 
   return dataHasChanged;