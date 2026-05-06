4c79c273e0e71901111a2f1c38d8721f06269801
Patrick Näf <herzbube@herzbube.ch>
2026-05-06 21:39:13 +0200

    update copyright year of files touched since the last release

--- src/main/SceneDelegate.m
+++ src/main/SceneDelegate.m
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------------
-// Copyright 2025 Patrick Näf (herzbube@herzbube.ch)
+// Copyright 2025-2026 Patrick Näf (herzbube@herzbube.ch)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
